4e8efad74ffaf4eccbac9f99db77d1cdf01adfa2
lillusory <xueduanli@163.com>
2019-05-15 19:23:22 +0800

    iota 条件语句

---
+++ src/learning/base/03_DataTypeConversion.go
@@ -0,0 +1,10 @@
+package mian
+
+import "fmt"
+
+func main() {
+	a := 90
+	b := 90.5
+	avg := ((float64(a)) + b) / 2
+	fmt.Println(avg)
+}
---
+++ src/learning/base/06_Const.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type user struct {
+	age  int
+	name string
+}
+
+func main() {
+	const NAME string = "illusory"
+	user := user{30, "illusory"}
+	const(
+		A=10
+		B
+		C
+		D=iota
+		E=iota
+	)
+	fmt.Println(user,A,B,C,D,E)
+}
