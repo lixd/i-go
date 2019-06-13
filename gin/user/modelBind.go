def14d2e49e15d1a449cacfc9647181a559398c8
lillusory <xueduanli@163.com>
2019-06-13 21:29:36 +0800

    fix

--- gin/user/modelBind.go
+++ gin/user/modelBind.go
@@ -11,7 +11,7 @@ type ModelBind struct {
 func (modelBind *ModelBind) BindHandler(c *gin.Context) {
 	var user = User{}
 	if err := c.Bind(&user); err == nil {
-		log.Printf("c.Bind() user.Name=%v user.Age=%v user.Address=%v", Name, Age, Address)
+		log.Printf("c.Bind() user.Name=%v user.Age=%v user.Address=%v", user.Name, user.Age, user.Address)
 	} else {
 		log.Printf("c.Bind() err=%v ", err)
 	}
@@ -19,7 +19,7 @@ func (modelBind *ModelBind) BindHandler(c *gin.Context) {
 func (modelBind *ModelBind) BindJSONHandler(c *gin.Context) {
 	var user = User{}
 	if err := c.BindJSON(&user); err == nil {
-		log.Printf("c.BindJSON() user.Name=%v user.Age=%v user.Address=%v", Name, Age, Address)
+		log.Printf("c.BindJSON() user.Name=%v user.Age=%v user.Address=%v", user.Name, user.Age, user.Address)
 	} else {
 		log.Printf("c.BindJSON() err=%v ", err)
 	}
@@ -27,7 +27,7 @@ func (modelBind *ModelBind) BindJSONHandler(c *gin.Context) {
 func (modelBind *ModelBind) ShouldBindHandler(c *gin.Context) {
 	var user = User{}
 	if err := c.ShouldBind(&user); err == nil {
-		log.Printf("c.ShouldBind() user.Name=%v user.Age=%v user.Address=%v", Name, Age, Address)
+		log.Printf("c.ShouldBind() user.Name=%v user.Age=%v user.Address=%v", user.Name, user.Age, user.Address)
 	} else {
 		log.Printf("c.ShouldBind() err=%v ", err)
 	}
@@ -35,7 +35,7 @@ func (modelBind *ModelBind) ShouldBindHandler(c *gin.Context) {
 func (modelBind *ModelBind) ShouldBindJSONHandler(c *gin.Context) {
 	var user = User{}
 	if err := c.ShouldBindJSON(&user); err == nil {
-		log.Printf("c.ShouldBindJSON() user.Name=%v user.Age=%v user.Address=%v", Name, Age, Address)
+		log.Printf("c.ShouldBindJSON() user.Name=%v user.Age=%v user.Address=%v", user.Name, user.Age, user.Address)
 	} else {
 		log.Printf("c.ShouldBindJSON() err=%v ", err)
 	}
