61e78f8b63279dbb5db1c32a549378f011af8dae
lixueduan <xueduanli@163.com>
2019-10-16 17:25:24 +0800

    grpc 提供http接口

--- grpc/server/hello_server.go
+++ grpc/server/hello_server.go
@@ -8,8 +8,9 @@ import (
 	"google.golang.org/grpc/status"
 	pb "i-go/grpc/proto"
 	"log"
-	"net"
+	"net/http"
 	"runtime/debug"
+	"strings"
 )
 
 const (
@@ -25,15 +26,44 @@ func (s *helloServer) SayHello(ctx context.Context, in *pb.HelloReq) (*pb.HelloR
 }
 
 func main() {
-	// 监听
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	s := grpc.NewServer(grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(LoggingInterceptor, RecoveryInterceptor)))
-	// 注册 server
-	pb.RegisterHelloServer(s, &helloServer{})
-	s.Serve(lis)
+	// RPC
+	// // 监听
+	// lis, err := net.Listen("tcp", port)
+	// if err != nil {
+	// 	log.Fatalf("failed to listen: %v", err)
+	// }
+	// s := grpc.NewServer(grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(LoggingInterceptor, RecoveryInterceptor)))
+	// // 注册 server
+	// pb.RegisterHelloServer(s, &helloServer{})
+	// s.Serve(lis)
+
+	// 	提供http接口
+	// http://localhost:50054/http -->xxx: gRPC 提供http接口
+	mux := GetHTTPServeMux()
+	s := grpc.NewServer(grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(LoggingInterceptor)))
+	pb.RegisterHelloServer(s, new(helloServer))
+
+	err := http.ListenAndServe("0.0.0.0:50054", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 根据Header判断是否为grpc请求
+		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
+			s.ServeHTTP(w, r)
+		} else {
+			mux.ServeHTTP(w, r)
+		}
+
+		return
+	}))
+	panic(err)
+}
+
+func GetHTTPServeMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/http", func(w http.ResponseWriter, r *http.Request) {
+		// 添加逻辑
+		w.Write([]byte("xxx: gRPC 提供http接口"))
+	})
+
+	return mux
 }
 
 func LoggingInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
