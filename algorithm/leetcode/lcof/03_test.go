d6d25c5d97c8cd611795659009389a3a15374df1
lixd <xueduan.li@gmail.com>
2021-07-17 12:55:04 +0800

    singleflight 源码分析

--- algorithm/leetcode/lcof/03_test.go
+++ algorithm/leetcode/lcof/03_test.go
@@ -12,6 +12,7 @@ func Test_findRepeatNumber(t *testing.T) {
 		want int
 	}{
 		{name: "1", args: args{[]int{2, 3, 1, 0, 2, 5, 3}}, want: 2},
+		{name: "2", args: args{[]int{1, 3, 1, 0, 2, 5, 3}}, want: 1},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
