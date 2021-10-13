df71941fb0f99c2dc63d385993ee7c15a9c4de97
lixd <xueduan.li@gmail.com>
2021-10-13 17:07:03 +0800

    sync.pool 复用临时对象

--- 17x/ctx/ctx.go
+++ 17x/ctx/ctx.go
@@ -6,9 +6,23 @@ import (
 	"time"
 )
 
+/*
+即时父协程超时返回了，但是子协程仍在继续运行，直到自己退出。
+并不能在父协程返回的时候直接 kill 子协程。
+
+goroutine 被设计为不可以从外部无条件地结束掉，只能通过 channel 来与它通信。也就是说，每一个 goroutine 都需要承担自己退出的责任。
+(A goroutine cannot be programmatically killed. It can only commit a cooperative suicide.)
+相关讨论见:https://github.com/golang/go/issues/32610
+摘抄其中几个比较有意思的观点如下：
+	1）杀死一个 goroutine 设计上会有很多挑战，当前所拥有的资源如何处理？堆栈如何处理？defer 语句需要执行么？
+	2）如果允许 defer 语句执行，那么 defer 语句可能阻塞 goroutine 退出，这种情况下怎么办呢？
+因为 goroutine 不能被强制 kill，在超时或其他类似的场景下，为了 goroutine 尽可能正常退出，建议如下：
+	1）尽量使用非阻塞 I/O（非阻塞 I/O 常用来实现高性能的网络库），阻塞 I/O 很可能导致 goroutine 在某个调用一直等待，而无法正确结束。
+	2）业务逻辑总是考虑退出机制，避免死循环。
+	3）任务分段执行，超时后即时退出，避免 goroutine 无用的执行过多，浪费资源。
+*/
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-
 	// consumer
 	go func(ctx context.Context) {
 		var ch = make(chan struct{})
---
+++ 17x/sync_pool/p_test.go
@@ -0,0 +1,76 @@
+package sync_pool
+
+import (
+	"bytes"
+	"encoding/json"
+	"sync"
+	"testing"
+)
+
+/*
+sync.pool 使用 https://zhuanlan.zhihu.com/p/133638023
+我们发现 Get 方法取出来的对象和上次 Put 进去的对象实际上是同一个，Pool 没有做任何“清空”的处理。但我们不应当对此有任何假设，
+因为在实际的并发使用场景中，无法保证这种顺序，最好的做法是在 Put 前，将对象清空。
+局部变量使用 sync.pool 可以降低分配压力，如果新建一个对象比较耗时则可以使用
+非局部变量使用 sync.pool 可以降低分配压力+gc压力，比如 gin 框架，会给每个请求都创建一个 context，就是用的 sync.pool 进行复用。
+*/
+var bufferPool = sync.Pool{
+	New: func() interface{} {
+		return &bytes.Buffer{}
+	},
+}
+
+var data = make([]byte, 10000)
+
+func BenchmarkBufferWithPool(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		buf := bufferPool.Get().(*bytes.Buffer)
+		buf.Write(data)
+		buf.Reset()
+		bufferPool.Put(buf)
+	}
+}
+
+func BenchmarkBuffer(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		var buf bytes.Buffer
+		buf.Write(data)
+	}
+}
+
+type Student struct {
+	Name   string
+	Age    int32
+	Remark [1024]byte
+}
+
+func (s *Student) Reset() {
+	s.Age = 0
+	s.Name = ""
+	s.Remark = [1024]byte{}
+}
+
+var studentPool = sync.Pool{
+	New: func() interface{} {
+		return new(Student)
+	},
+}
+
+var defaultStu, _ = json.Marshal(Student{Name: "Geektutu", Age: 25})
+var emptyStu = Student{}
+
+func BenchmarkUnmarshal(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		stu := &Student{}
+		json.Unmarshal(defaultStu, stu)
+	}
+}
+
+func BenchmarkUnmarshalWithPool(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		stu := studentPool.Get().(*Student)
+		json.Unmarshal(defaultStu, stu)
+		stu.Reset() // 将stu重置后在放进去
+		studentPool.Put(stu)
+	}
+}
